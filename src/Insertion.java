6c01ccab67b6e6c50928a163d50f8244f4d12333
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-25 11:22:43 +0530

    heaps

--- src/Insertion.java
+++ src/Insertion.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
+
 public class Insertion {
+
+    static class Heap {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        public void insert(int data) {
+            arr.add(data);
+
+            int x = arr.size() - 1;
+            int par = (x - 1) / 2;
+
+            while (arr.get(x) < arr.get(par)) {
+                // swap
+                int temp = arr.get(x);
+                arr.set()
+            }
+        }
+    }
 
 
     public static void main(String[] args) {
