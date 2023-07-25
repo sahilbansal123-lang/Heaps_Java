56f32336d2db582ef3efe5f33908df641fb37f5d
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-25 11:57:59 +0530

    heaps

--- src/Insertion.java
+++ src/Insertion.java
@@ -5,22 +5,28 @@ public class Insertion {
     static class Heap {
         ArrayList<Integer> arr = new ArrayList<>();
 
+        // Time Complexity = O(log n)
         public void insert(int data) {
             arr.add(data);
 
-            int x = arr.size() - 1;
-            int par = (x - 1) / 2;
+            int x = arr.size() - 1; // child index
+            int par = (x - 1) / 2;  // parent index
 
             while (arr.get(x) < arr.get(par)) {
                 // swap
                 int temp = arr.get(x);
-                arr.set()
+                arr.set(x, arr.get(par)); // x index pr par index ke element ko add
+                arr.set(par, temp);       // par ke index pr x ke index ke element ko add
             }
+        }
+
+        public void peek() {
+            arr.get(0);
         }
     }
 
 
     public static void main(String[] args) {
-
+        ArrayList<Integer> arr = new ArrayList<>();
     }
 }
