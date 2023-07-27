b8a2a410aeff84620675497fc38b6437ec838d28
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-27 10:58:39 +0530

    heaps

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,7 +1,24 @@
 public class HeapSort {
 
     public static void heapSort (int arr[]) {
-        // Step1
+        // Step1 --> Build heap
+        int n = arr.length;
+        for (int i = n/2; i >= 0; i--) {
+            heapify(arr, i, n);
+        }
+
+        // step2 --> Push largest at end
+        for (int i = n-1; i >= 0; i--) {
+            // Swap First with last
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, 0, i);
+        }
+    }
+
+    private static void heapify(int[] arr, int i, int size) {
     }
 
     public static void main(String[] args) {
